b8bb93b00892fc94c1d87dd20e9501eb63f60d3e
Jimin Park <ujpjm21@gmail.com>
2025-11-07 00:33:23 -0500

    Time: 7 ms (97.81%), Space: 18.3 MB (21.25%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,8 +1,17 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         n = len(senate)
-        R = deque(i for i, c in enumerate(senate) if c == 'R')
-        D = deque(i for i, c in enumerate(senate) if c == 'D')
+        R = []
+        D = []
+        for i, c in enumerate(senate):
+            if c == 'R':
+                R.append(i)
+            else:
+                D.append(i)
+
+        from collections import deque
+        R = deque(R)
+        D = deque(D)
 
         while R and D:
             r = R.popleft()
@@ -12,4 +21,3 @@ class Solution:
             else:
                 D.append(d + n)
         return "Radiant" if R else "Dire"
-        
