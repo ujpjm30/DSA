f07cc16a24fb3da2c47637417c66a43585f63477
Jimin Park <jimin.park.mlds@gmail.com>
2026-02-25 00:57:25 -0500

    Time: 15 ms (16.14%), Space: 19.7 MB (44.4%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -4,10 +4,9 @@ class Solution:
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0:
                 left = (i == 0) or (flowerbed[i - 1] == 0)
-                right = (i == len(flowerbed) - 1) or (flowerbed[i +1] == 0)
+                right = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                
                 if left and right:
                     flowerbed[i] = 1
                     count += 1
         return count >= n
-
-        
