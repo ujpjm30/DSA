55027e29c0a2f1f54844f66b690580c802c08a42
Jimin Park <jimin.park.mlds@gmail.com>
2026-02-26 15:13:40 -0500

    Time: 0 ms (100%), Space: 19.4 MB (36.24%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,6 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+         from collections import Counter
+         count = Counter(arr)
+         return len(set(count.values())) == len(count)
+        
