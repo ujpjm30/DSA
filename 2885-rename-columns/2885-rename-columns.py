670f082df5198c186a94685877d490abffa0535d
Jimin Park <jimin.park.mlds@gmail.com>
2026-08-20 13:42:48 -0400

    Time: 330 ms (6.98%), Space: 66.4 MB (64.51%) - LeetHub

---
+++ 2885-rename-columns/2885-rename-columns.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(
+
+        columns={
+
+            "id": "student_id",
+
+            "first": "first_name",
+
+            "last": "last_name",
+
+            "age": "age_in_years"
+
+        }
+
+    )
+    
