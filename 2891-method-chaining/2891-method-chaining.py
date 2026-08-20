f28d8aff11360485e5880c46ab3839784a40fa07
Jimin Park <jimin.park.mlds@gmail.com>
2026-08-20 16:48:12 -0400

    Time: 283 ms (76.29%), Space: 67 MB (66.6%) - LeetHub

--- 2891-method-chaining/2891-method-chaining.py
+++ 2891-method-chaining/2891-method-chaining.py
@@ -2,11 +2,5 @@
 import pandas as pd
 
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
-    return animals.loc[animals["weight"] > 100].sort_values(
-
-        by="weight",
-
-        ascending=False
-
-    )[["name"]]
+    return animals.loc[animals["weight"] > 100].sort_values(by="weight", ascending=False)[["name"]]
     
