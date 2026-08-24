cb63de6637fcc2fced9dd58178459c5f31bec094
Jimin Park <jimin.park.mlds@gmail.com>
2026-08-24 16:31:30 -0400

    Time: 10 ms (86.81%), Space: 22 MB (79.1%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+
+        groups = defaultdict(list)
+
+        for word in strs:
+
+            key = ''.join(sorted(word))
+
+            groups[key].append(word)
+
+        return list(groups.values())
+        
